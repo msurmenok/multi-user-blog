6819351cf19d7780522b1578c4548dfb0c5cc30c
Mariia <mariasurmenok@gmail.com>
2017-04-25 14:58:27 -0700

    added base elements (structure) to backend and base template

--- main.py
+++ main.py
@@ -11,17 +11,18 @@ import jinja2
 from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
-                               autoescape = True)
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
+                               autoescape=True)
+
+
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
 
 
 class BlogHandler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
-
-    def render_str(template, **params):
-        t = jinja_env.get_template(template)
-        return t.render(params)
 
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
@@ -33,10 +34,104 @@ class BlogHandler(webapp2.RequestHandler):
 
 
 class MainPage(BlogHandler):
-  def get(self):
-      self.write('Hello Blog')
+    def get(self):
+        """ Show 10 newest posts"""
+        self.write('Hello Blog')
 
 
+class NewPost(BlogHandler):
+    # if not logged in -> redirect to login page
+    pass
+
+
+class ViewPost(BlogHandler):
+    pass
+
+
+class Signup(BlogHandler):
+    def get(self):
+        self.render("signup-form.html")
+
+    def post(self):
+        have_error = False
+        self.username = self.request.get('username')
+        self.password = self.request.get('password')
+        self.verify = self.request.get('verify')
+        self.email = self.request.get('email')
+
+        params = dict(username=self.username,
+                      email=self.email)
+
+        if not valid_username(self.username):
+            params['error_username'] = "That's not a valid username."
+            have_error = True
+
+        if not valid_password(self.password):
+            params['error_password'] = "That wasn't a valid password."
+            have_error = True
+        elif self.password != self.verify:
+            params['error_verify'] = "Your passwords didn't match."
+            have_error = True
+
+        if not valid_email(self.email):
+            params['error_email'] = "That's not a valid email."
+            have_error = True
+
+        if have_error:
+            self.render('signup-form.html', **params)
+        else:
+            # redirect to welcome page
+            pass
+
+
+class Login(BlogHandler):
+    # redirect to welcome page
+    pass
+
+
+class Logout(BlogHandler):
+    pass
+
+
+class Welcome(BlogHandler):
+    pass
+
+
+# DB ENTITIES
+class BlogPost(db.Model):
+    title = db.StringProperty(required=True)
+    content = db.TextProperty(required=True)
+    author_id = db.IntegerProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+    last_modified = db.DateTimeProperty(auto_now=True)
+
+
+class User(db.Model):
+    username = db.StringProperty(required=True)
+    hash = db.StringProperty(required=True)
+    salt = db.StringProperty(required=True)
+
+
+# Helper functions
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+PASS_RE = re.compile(r"^.{3,20}$")
+EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+
+
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+
+def valid_password(password):
+    return password and PASS_RE.match(password)
+
+
+def valid_email(email):
+    return not email or EMAIL_RE.match(email)
+
+
+# Routing
 app = webapp2.WSGIApplication([('/', MainPage),
+                               ('/signup', Signup)
                                ],
                               debug=True)
