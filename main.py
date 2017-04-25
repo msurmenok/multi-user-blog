3b5f79fde9e4d96454cfa80074212040e53c0d9c
Mariia <mariasurmenok@gmail.com>
2017-04-24 17:17:55 -0700

    initial structure

---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+import re
+import random
+import hashlib
+import hmac
+from string import letters
+
+import webapp2
+import jinja2
+
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+
+
+class BlogHandler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+"""    def initialize(self, *a, **kw):
+        webapp2.RequestHandler.initialize(self, *a, **kw)
+        uid = self.read_secure_cookie('user_id')
+        self.user = uid and User.by_id(int(uid))"""
+
+
+class MainPage(BlogHandler):
+  def get(self):
+      self.write('Hello Blog')
+
+
+app = webapp2.WSGIApplication([('/', MainPage),
+                               ],
+                              debug=True)
